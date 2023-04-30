1091f96881040fa8796f5b90cd944c7d9ab2923a
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-04-30 15:15:52 -0400

    notes scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_30_183750) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_30_190621) do
+  create_table "notes", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -19,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_30_183750) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "notes", "users"
 end
