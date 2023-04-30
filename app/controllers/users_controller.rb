3d4254c488869cefa4fa5e5b2d646d279f123c72
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-04-30 15:05:21 -0400

    able to create a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,26 @@ class UsersController < ApplicationController
             render json: {user: @user, token: token}
         else
             render json: {error: "Invalid username or password"}
+        end
+    end
+    
+    # Logging In
+    def login
+        @user = User.find_by(username: params[:username])
 
+        if @user && @user.authenticate(params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+        end
+    end
 
+    def auto_login
+        render json: @user
+    end
 
+    private
 
     def user_params
         params.permit(:username, :password, :age)
