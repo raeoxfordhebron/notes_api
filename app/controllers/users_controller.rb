42486064ca3ef049fcf07090137b350b0ffcf917
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-04-30 14:56:59 -0400

    application controller has authorization functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
-    
+    before_action :authorized, only: [:auto_login]
+
+    # Register
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+
+
+
+
+    def user_params
+        params.permit(:username, :password, :age)
+    end
 end
