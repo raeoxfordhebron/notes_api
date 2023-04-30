705f9a1c76d15b77528e21a329f545df45b43afd
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-04-30 14:41:12 -0400

    migration for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
   # Defines the root path route ("/")
   # root "articles#index"
 end
